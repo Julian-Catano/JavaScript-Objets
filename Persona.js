96f22a1bcb6ef474b333ef1cf899f556f4ad3419
Julian Cataño <julianestivenposso@gmail.com>
2024-08-02 16:10:10 -0500

    Realizacion del proceso de desarrollo

--- Persona.js
+++ Persona.js
@@ -1,20 +1,47 @@
-class Persona{
-    #nombre=''
-    #edad=''
-    #fechaNacimiento=''
-    constructor(nombre, edad, fechaNacimiento){
-        this.#nombre = nombre;
-        this.#edad = edad;
-        this.#fechaNacimiento = fechaNacimiento;
+obtenerNombre(nombre){
+
+    if(nombre !== ""){
+        return this.#nombre
+    }else{
+        alert("Ta vacio manetoo")
     }
-
-    establecerNombre(nombre){
-        if(nombre !== ""){
-            this.#nombre=nombre
-        }else{
-            return alert("Brodel ta vacio")
-        }
-    }
-
-
+    
 }
+
+establecerEdad(edad){
+
+    if(edad < 0){
+        alert("No se puede esa edad brodel")
+    }else{
+        this.#edad=edad
+    } // La edad no puede ser negativa y menor a 18 años
+   
+}
+obtenerEdad(edad){
+    if(edad < 0){
+        alert("No se puede esa edad brodel")
+    }else{
+        return this.#edad=edad
+    }
+}
+
+establecerFechaNacimiento(fechaNacimiento){
+
+    if(fechaNacimiento > new Date()){
+        alert("No se puede esa fecha brodel")
+    } else{
+        this.#fechaNacimiento=fechaNacimiento
+    } 
+    
+}
+
+obtenerFechaNacimiento(fechaNacimiento){
+    if(fechaNacimiento > new Date()){
+        alert("No se puede esa fecha brodel")
+    } else{
+        return this.#fechaNacimiento=fechaNacimiento
+    } 
+}
+
+
+
