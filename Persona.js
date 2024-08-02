32caccd3e34b0ecc50a4bd375fbae3d16a3cfecb
Julian Cataño <julianestivenposso@gmail.com>
2024-08-02 16:03:33 -0500

    establecerNombre

---
+++ Persona.js
@@ -0,0 +1,20 @@
+class Persona{
+    #nombre=''
+    #edad=''
+    #fechaNacimiento=''
+    constructor(nombre, edad, fechaNacimiento){
+        this.#nombre = nombre;
+        this.#edad = edad;
+        this.#fechaNacimiento = fechaNacimiento;
+    }
+
+    establecerNombre(nombre){
+        if(nombre !== ""){
+            this.#nombre=nombre
+        }else{
+            return alert("Brodel ta vacio")
+        }
+    }
+
+
+}
