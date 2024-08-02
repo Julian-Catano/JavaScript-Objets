01eac8d3502f6973ef2569a75d5a48adb2d8da53
Julian Cataño <julianestivenposso@gmail.com>
2024-08-02 17:03:47 -0500

    inicializar propiedades heredadas con el constructor de la clase estudiante

--- Estudiante.js
+++ Estudiante.js
@@ -2,13 +2,13 @@ const Persona = require('./Persona.js')
 
 class Estudiante extends Persona {
     constructor(nombre, edad, fechaNacimiento, grado){
+        super(nombre, edad, fechaNacimiento);
         this.grado = grado;
     }
 
     toString(){
         return `${super.toString()}, Estudiante: ${this.Estudiante}`;
     }
-
-
+    
 
 }
