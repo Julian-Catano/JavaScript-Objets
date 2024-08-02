609de5ed1246c5aa9443aeb9f1441ebf75391e2a
Julian Cataño <julianestivenposso@gmail.com>
2024-08-02 16:13:28 -0500

    metodo obtener nombre

--- Persona.js
+++ Persona.js
@@ -1,47 +1,27 @@
-obtenerNombre(nombre){
-
-    if(nombre !== ""){
-        return this.#nombre
-    }else{
-        alert("Ta vacio manetoo")
+class Persona{
+    #nombre=''
+    #edad=''
+    #fechaNacimiento=''
+    constructor(nombre, edad, fechaNacimiento){
+        this.#nombre = nombre;
+        this.#edad = edad;
+        this.#fechaNacimiento = fechaNacimiento;
     }
-    
-}
 
-establecerEdad(edad){
-
-    if(edad < 0){
-        alert("No se puede esa edad brodel")
-    }else{
-        this.#edad=edad
-    } // La edad no puede ser negativa y menor a 18 años
-   
-}
-obtenerEdad(edad){
-    if(edad < 0){
-        alert("No se puede esa edad brodel")
-    }else{
-        return this.#edad=edad
+    establecerNombre(nombre){
+        if(nombre !== ""){
+            this.#nombre=nombre
+        }else{
+            return alert("Brodel ta vacio")
+        }
     }
-}
+    obtenerNombre(nombre){
 
-establecerFechaNacimiento(fechaNacimiento){
 
-    if(fechaNacimiento > new Date()){
-        alert("No se puede esa fecha brodel")
-    } else{
-        this.#fechaNacimiento=fechaNacimiento
-    } 
+        
+    }
+
+
     
+
 }
-
-obtenerFechaNacimiento(fechaNacimiento){
-    if(fechaNacimiento > new Date()){
-        alert("No se puede esa fecha brodel")
-    } else{
-        return this.#fechaNacimiento=fechaNacimiento
-    } 
-}
-
-
-
