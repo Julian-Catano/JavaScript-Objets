94b9c31a71598b332f4ba4d7b5870febc518d7b4
Julian Cataño <julianestivenposso@gmail.com>
2024-08-03 01:39:04 -0500

    Implementacion de subclases

--- Emplea2.js
+++ Emplea2.js
@@ -4,5 +4,26 @@ class Emplea2{
         this.salario = salario;
     }
 
+
+}
+
+class Gerente extends Emplea2{
+    constructor(nombre, salario, bono){
+        super(nombre, salario);
+        this.bono = bono;
+    }
+    
+
+
+
     
 }
+class Vendedor extends Emplea2{
+    constructor(nombre, salario, comision){
+        super(nombre, salario);
+        this.comision = comision;
+    }
+
+
+}
+
