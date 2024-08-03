dcb329b4ba418a2aafeb57b8244dc7747028ad75
Julian Cataño <julianestivenposso@gmail.com>
2024-08-03 01:42:01 -0500

    Herencia y Polimorfismo - Implementación del Método calcular salario en clase(Gerente y Vendedor)

--- Emplea2.js
+++ Emplea2.js
@@ -14,7 +14,13 @@ class Gerente extends Emplea2{
     }
     
 
+    calcularSueldoG(nombre){
+        return this.salario + this.bono;
+    }
 
+    toString(){
+        return `${super.toString()}, salario: ${this.salario}`;
+    }
 
     
 }
@@ -24,6 +30,16 @@ class Vendedor extends Emplea2{
         this.comision = comision;
     }
 
+    calcularSueldoV(){
+        return this.nombre, this.salario + this.comision
+    }
 
+    toString(){
+        return `${super.toString()}, salario: ${this.salario}`;
+    }
 }
 
+const gerente = new Gerente("julian", 1300000, 300000)
+console.log(gerente.calcularSueldoG())
+const vendedor = new Vendedor("estiven", 1300000, 30000)
+console.log(vendedor.calcularSueldoV())
